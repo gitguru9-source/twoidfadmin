e1554c77d93f5e42cc0df8988c5a55885b2ffcce
gitguru9-source <gitguru9@gmail.com>
2025-11-20 20:25:59 +0000

    defgtrf

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1994787877"
+    serviceWorkerVersion: "4232259812"
   }
 });
